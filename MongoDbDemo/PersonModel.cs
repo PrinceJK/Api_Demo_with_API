7905d30e3895402e0c4b4204a735f3d85774f62f
John Kehinde <kehindejohn018@gmail.com>
2021-09-17 19:26:17 +0100

    Mongo Db Demo

---
+++ MongoDbDemo/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbDemo
+{
+    public class PersonModel
+    {
+        [BsonId] //Id
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AddressModel PrimaryAddress { get; set; }
+        [BsonElement("dob")]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ MongoDbDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MongoDbDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AdderessBook");
+
+            //PersonModel person = new PersonModel
+            //{
+            //    FirstName = "Joe",
+            //    LastName = "Smith",
+            //    PrimaryAddress = new AddressModel
+            //    {
+            //        StreetAddress = "101 Oak Street",
+            //        City = "Scranton",
+            //        State = "PA",
+            //        ZipCode = "26536"
+            //    }
+            //};
+
+
+
+
+
+            //db.InsertRecord("Users", person);
+            var recs = db.LoadRecord<NameModel>("Users");
+
+            foreach (var rec in recs)
+            {
+                Console.WriteLine($"{rec.FirstName} {rec.LastName}");
+                Console.WriteLine();
+            }
+
+            //foreach (var rec in recs)
+            //{
+            //    Console.WriteLine($"{rec.Id}: {rec.FirstName} {rec.LastName}");
+
+            //    if (rec.PrimaryAddress != null)
+            //    {
+            //        Console.WriteLine(rec.PrimaryAddress.City);
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            //var oneRec = db.LoadRecordById<PersonModel>("Users", new Guid("510072ad-0578-4576-9d55-f609aa15458a"));
+            //oneRec.DateOfBirth = new DateTime(1992, 10, 31, 0, 0,0, DateTimeKind.Utc);
+            //db.UpsertRecord("Users", oneRec.Id, oneRec);
+            //db.DeleteRecord<PersonModel>("Users", oneRec.Id);
+            Console.ReadLine();
+        }
+    }
+}
